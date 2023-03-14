447d587adada25e562fb14ee5d520905cd5130f6
Alejandra-333 <94836604+Alejandra-333@users.noreply.github.com>
2023-03-14 17:15:23 +0100

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ns-app',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.componenent.css'],
 })
 export class AppComponent {}
